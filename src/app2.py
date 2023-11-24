380497fcef064087091c24cc770675150880dc6d
Gerardo Villafana <lg06luisgerardo@gmail.com>
2023-11-23 20:52:42 -0600

    23/11/2023---lgvc

--- src/app2.py
+++ src/app2.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 from flask_mysqldb import MySQL
 
@@ -27,6 +27,58 @@ def listar_alumnos():
         return jsonify({'alumnos':listar_alumnos,'mensaje':'Lista de alumnos '})
     except Exception as ex:
         return jsonify({'mensaje': 'error de conexion'})
+    
+
+def leer_alumno_bd(mat):
+      try:  
+        cursor=con.connection.cursor()
+        sql="select * from alumnos where matricula={0}".format(mat)
+        cursor.execute(sql)
+        datos=cursor.fetchone()
+        if datos!=None:
+         alumno={'matricula':datos[0],
+                        'nombre':datos[1],
+                        'apaterno':datos[2],
+                        'amaterno':datos[3],
+                        'correo':datos[4]}
+         return alumno
+        else:
+                return None
+      except Exception as ex:
+        raise ex
+      
+@app.route('/alumnos/<mat>',methods=['GET'])
+def leer_alumno(mat):
+    try:
+        alumno=leer_alumno_bd(mat)
+        if alumno !=None:
+            return jsonify({'alumno':alumno,'mensaje':'Alumno encontraso.','Exito':True})      
+        else:
+            return jsonify({'mensaje':'error de conexion','Exito':False})            
+    except Exception as ex:
+            return jsonify({'mensaje':'error de conexion {}'.format(ex)}) 
+
+
+
+@app.route('/alumnos',methods=['POST'])
+def registrar_alumno():
+    try:
+        alumno=leer_alumno_bd(request.json['matricula'])
+        if alumno!=None:
+             return jsonify({'mensaje':'Alumno ya existe','Exito':False})
+        else:
+            cursor=con.connection.cursor()
+            sql=""""INSERT INTO alumnos(matricula, nombre, apaterno, amaterno, correo)
+            VALUES({0},'{1}','{2}','{3}','{4}')""".format(request.json['matricula'],
+            request.json['nombre'],request.json['apaterno'],request.json['amaterno'],request.json['correo'])
+            cursor.execute(sql)
+            con.connection.commit()            
+        return jsonify({'mensaje':'Alumno registrado exitosamente','exito':True})   
+    except Exception as ex:
+            return jsonify({'mensaje':'error de conexion {}'.format(ex)}) 
+
+
+
 
 def pagina_no_encontrada(error):
     return "<h1> pagina no encontrada.....</h1>", 404

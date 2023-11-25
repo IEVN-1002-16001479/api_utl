b3f3b077d54ca3312cba75237c627448ba13229f
Gerardo Villafana <lg06luisgerardo@gmail.com>
2023-11-25 17:51:40 -0600

    25/11/2023---lgvc

--- src/app2.py
+++ src/app2.py
@@ -60,7 +60,7 @@ def leer_alumno(mat):
 
 
 
-@app.route('/alumnos',methods=['POST'])
+@app.route('/alumnos', methods=['POST'])
 def registrar_alumno():
     try:
         alumno=leer_alumno_bd(request.json['matricula'])
@@ -68,7 +68,7 @@ def registrar_alumno():
              return jsonify({'mensaje':'Alumno ya existe','Exito':False})
         else:
             cursor=con.connection.cursor()
-            sql=""""INSERT INTO alumnos(matricula, nombre, apaterno, amaterno, correo)
+            sql="""INSERT INTO alumnos (matricula, nombre, apaterno, amaterno, correo)
             VALUES({0},'{1}','{2}','{3}','{4}')""".format(request.json['matricula'],
             request.json['nombre'],request.json['apaterno'],request.json['amaterno'],request.json['correo'])
             cursor.execute(sql)
@@ -76,8 +76,47 @@ def registrar_alumno():
         return jsonify({'mensaje':'Alumno registrado exitosamente','exito':True})   
     except Exception as ex:
             return jsonify({'mensaje':'error de conexion {}'.format(ex)}) 
+    
 
 
+@app.route('/alumnos/<mat>',methods=['PUT'])
+def modificar_alumno(mat):
+    try:
+        alumno=leer_alumno_bd(mat)
+        if alumno !=None:
+            cursor=con.connection.cursor()
+            sql="""UPDATE  alumnos SET nombre='{0}', 
+            apaterno='{1}', 
+            amaterno='{2}', 
+            correo='{3}' 
+            WHERE matricula={4}""".format(request.json['nombre'],
+            request.json['apaterno'],
+            request.json['amaterno'],
+            request.json['correo'], mat)
+            cursor.execute(sql)
+            con.connection.commit()       
+            return jsonify({'mensaje':'Alumno actualizado exitosamente','exito':True})
+        else:
+            return jsonify({'mensaje':'Alumno no encontrado','Exito':False})            
+    except Exception as ex:
+            return jsonify({'mensaje':'error de conexion {}'.format(ex)})
+
+
+@app.route('/alumnos/<mat>',methods=['DELETE'])
+def eliminar_alumno(mat):
+    try:
+        alumno=leer_alumno_bd(mat)
+        if alumno !=None:
+            cursor=con.connection.cursor()
+            sql="""DELETE FROM alumnos WHERE matricula={0}""".format(mat)
+            cursor.execute(sql)
+            con.connection.commit()       
+            return jsonify({'mensaje':'Alumno eliminado exitosamente','exito':True})
+        
+        else:
+            return jsonify({'mensaje':'Alumno no encontrado','Exito':False})            
+    except Exception as ex:
+            return jsonify({'mensaje':'error de conexion {}'.format(ex)})     
 
 
 def pagina_no_encontrada(error):
